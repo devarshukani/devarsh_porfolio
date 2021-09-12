9e52eb36468a99be38dca017954c17cd7756f59b
Devarsh Ukani <me.devarshukani@gmail.com>
2021-09-12 21:56:13 +0530

    projects card auto play stop

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ $(document).ready(function(){
     $('.carousel').owlCarousel({
         margin: 20,
         // loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
+        //autoplayTimeOut: 2000,
+        //autoplayHoverPause: true,
         responsive: {
             0:{
                 items: 1,

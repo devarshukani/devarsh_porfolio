bc2dfd52d340f9b567b890e8f9952f21377e9801
Devarsh Ukani <me.devarshukani@gmail.com>
2021-10-30 10:07:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["App Developer", "Web Developer"],
+        strings: ["Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
